45f2c352526d5f055a075bae7b394af7a17eb5d9
Prajwal Prakash <prajwalpy6@gmail.com>
2015-07-13 22:09:15 +0530

    Corrected synatx error in paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_host_name => 's3-ap-southeast-1.amazonaws.com'
+    :s3_host_name => 's3-ap-southeast-1.amazonaws.com',
     :s3_credentials => {
       :s3_protocol => 'http',
       :bucket => 'storyhoot-gif-uploads', 
