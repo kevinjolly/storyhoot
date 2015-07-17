d5a95412f0a1c2ddc5d9efad10decc2d2fd1385d
Prajwal Prakash <prajwalpy6@gmail.com>
2015-07-17 19:41:11 +0530

    Notification number displays on the tab as title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,16 @@ module ApplicationHelper
     Category.all
   end
 
+  def full_title
+    if current_user
+      unread_count = current_user.bulletins.unread_by(current_user).count
+      if unread_count > 0
+        return 'Storyhoot (' + current_user.bulletins.unread_by(current_user).count.to_s + ')'
+      end
+    end
+    return 'Storyhoot! Share your story with the world'
+  end
+
   def auto_link(content)
 
     usernames = extract_mentioned_screen_names(content)
